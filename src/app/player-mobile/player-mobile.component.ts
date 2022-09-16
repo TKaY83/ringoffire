c32c54f70e0873e6761d50f5955c527b3ae9020e
Tuncay Dagdelen <sramos083@gmail.com>
2022-09-16 15:28:23 +0200

    Game is Responsive

--- src/app/player-mobile/player-mobile.component.ts
+++ src/app/player-mobile/player-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player-mobile',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-mobile.component.scss']
 })
 export class PlayerMobileComponent implements OnInit {
-
+  @Input() name;
+  @Input() playerActive: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
